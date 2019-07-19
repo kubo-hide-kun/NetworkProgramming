9f9b8c09f799f7b25cdf876c1bd3832d57af1c37
kubo-hide-kun <hide44041@gmail.com>
2019-07-19 10:23:42 +0900

    Add support for receiving HTTP requiest

--- www/server.c
+++ www/server.c
@@ -5,6 +5,7 @@
 #include <string.h>             /* strlen()に必要 */
 #include <unistd.h>             /* close()に必要 */
 #include <string.h>             /* stringに必要 */
+#include <regex.h>              /* 正規表現に必要 */
 #define BUF_SIZE 512            /* マジックナンバーで512の定義 */
 
 void DieWithError(char *errorMessage){
@@ -18,15 +19,41 @@ void commun(int sock){
     char buf2[2*BUF_SIZE];                                                              /* エコー文字列用のバッファ */
     int len_r;                                                                          /* 受信文字数 */
     char response[BUF_SIZE];
+    char result[100];
+    char *uri;
+    result[0]='\0';
+    regex_t regBuf;                                                                     /* 正規表現オブジェクトを保存 */
+    regmatch_t regMatch[1];                                                             /* 1個だけ検索結果を保存 */
+
+    const char *pattern = "GET.+HTTP";                                                  /* 正規表現文字列 */
+    if(regcomp(&regBuf,pattern,REG_EXTENDED|REG_NEWLINE))                               /* 正規表現文字列を正規表現オブジェクトに変換 */
+        DieWithError("regcomp failed");
 
     buf_old[0] = '\0';
 
     while((len_r=recv(sock,buf,BUF_SIZE,0))>0){
         buf[len_r] = '\0';                                                              /* 文末EOSの追加 */
         sprintf(buf2, "%s%s\n", buf_old, buf);
+
+        if(regexec(&regBuf,buf2,1,regMatch,0)){                                         /* 正規表現にHITしたか? */
+            int startIndex = regMatch[0].rm_so;                                         /* HITした地点の開始位置 */
+            int endIndex = regMatch[0].rm_eo;                                           /* HITした地点の終了位置 */
+            printf("%d %d\n",startIndex,endIndex);
+            strncpy(result,buf2+startIndex,endIndex-startIndex);                        /* 開始位置から終了位置までの文字列をresultに格納 */
+        }
+
         if(strstr(buf2, "\r\n\r\n"))break;
         sprintf(buf_old,"%s",buf);
     }
+
+    if(result[0]!='\n'){
+        uri = strtok(result, " ");
+        uri = strtok(NULL," ");
+        printf("%s\n",uri);
+    }else{
+        DieWithError("No URI");
+    }
+    regfree(&regBuf);                                                                   /* 正規表現オブジェクトを解放 */
 
     if(len_r <= 0)DieWithError("recieved() failed.\n");
     printf("received HTTP Request.\n");
@@ -51,7 +78,6 @@ void commun(int sock){
 }
 
 int main(int argc, char **argv) {
-    printf("start!\n");
     int servSock = socket(PF_INET,SOCK_STREAM,0);                                       /* 待ち受け用ソケットの作成(PF_INET=IPv4,SOCKET_STREAM=TCP,0=残りはお任せ) */
 
     int cliSock;                                                                        /* 通信用のソケット */
@@ -68,7 +94,6 @@ int main(int argc, char **argv) {
     while(1){
         szClientAddr = sizeof(clientAddress);                                           /* 受信データの形式のメモリサイズを取得 */
         cliSock = accept(servSock,(struct sockaddr*)&clientAddress,&szClientAddr);      /*  */
-        printf("commun!\n");
         commun(cliSock);                                                                /* ユーザー定義 */
         close(cliSock);                                                                /* serverをクローズする */
     }
